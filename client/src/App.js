79315cb51db7710a0ad24f096ca46c18aca838bf
kristinacarlsen <kristida84@gmail.com>
2020-01-11 20:49:02 -0700

    adds text

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my test app. 
+          This is my test app. Changing it up to push the changes.
         </p>
         <a
           className="App-link"
